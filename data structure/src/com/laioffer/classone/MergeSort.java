e67c251d579fe3bceba6092df4085a3e5c7f915b
sally9805 <haishangao2020@u.northwestern.edu>
2018-07-09 23:24:20 -0500

    mergeSort

--- data structure/src/com/laioffer/classone/MergeSort.java
+++ data structure/src/com/laioffer/classone/MergeSort.java
@@ -1,5 +1,16 @@
 package com.laioffer.classone;
 
 public class MergeSort {
-
+	public static void main(String[] args) {
+		int[] array = {3,1,2,4,5};
+		int[] sorted = mergeSort(array);
+		for (int i = 0;i<sorted.length;i++) {
+			System.out.print(sorted[i]);
+		}
+	}
+	public static int[] mergeSort(int[] array) {
+		int mid = array.length/2;
+		int left = helper(array,0,mid);
+		int right = helper(array,mid+1,array.length-1);
+	}
 }
