1d976c09d9bdc77f5d003a78467204d0de6eaa2b
sally9805 <haishangao2020@u.northwestern.edu>
2018-09-29 17:29:43 -0500

    update

---
+++ data structure/src/com/laioffer/classone/quickSort.java
@@ -0,0 +1,14 @@
+package com.laioffer.classone;
+
+public class quickSort {
+	public static void main(String[] args) {
+		int[] array = {3,1,2,6,5};
+		int[] sort = quickSort(array);
+		for (int i = 0;i<array.length;i++) {
+			System.out.print(array[i]);
+		}
+	}
+	public static int[] quickSort(int[] array) {
+		return array;
+	}
+}
